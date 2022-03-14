a02c7a693046bdd987d37ca62b89cf32af4b3e87
Joe Mulholland <jmulholl@usc.edu>
2022-03-14 12:46:39 -0400

    minor fixes

--- app/src/main/java/com/example/project2/LoginActivity.java
+++ app/src/main/java/com/example/project2/LoginActivity.java
@@ -53,56 +53,56 @@ public class LoginActivity extends AppCompatActivity {
         successLogin = true; // reset success flag
     }
 
-//    public void onClickSignUp(View view) {
-//        EditText usernameField = (EditText)findViewById(R.id.usernameSignupBox);
-//        EditText passwordField = (EditText)findViewById(R.id.passwordSignupBox);
-//        EditText nameField = (EditText)findViewById(R.id.nameSignupBox);
-//        EditText emailField = (EditText)findViewById(R.id.emailLoginBox);
-//        String username = usernameField.getText().toString();
-//        String password = passwordField.getText().toString();
-//        String name = nameField.getText().toString();
-//        String email = emailField.getText().toString();
-//        if (username.length() == 0) {
-//            usernameField.setError("Please enter your username.");
-//            successSignup = false;
-//        }
-//        // ELSE IF USERNAME EXISTS IN DB, ERROR
-//
-//        if (password.length() == 0) {
-//            passwordField.setError("Please enter your password.");
-//            successSignup = false;
-//        }
-//        else if (password.length() < 8) {
-//            passwordField.setError("Password must be at least 8 characters.");
-//            successSignup = false;
-//        }
-//        if (name.length() == 0) {
-//            nameField.setError("Please enter your name.");
-//            successSignup = false;
-//        }
-//        if(email.length() == 0) {
-//            emailField.setError("Please enter your email.");
-//            successSignup = false;
-//        }
-//        // ELSE IF EMAIL EXISTS IN DB, ERROR
-//
-//        if (successSignup) {
-//            signupUser = new User(name, email, username, password);
-//            // ADD USER TO DATABASE AND GET USER ID
-//            // User u = GET USER FROM DB BY USER ID
-//            Intent i = new Intent(LoginActivity.this, ProfileActivity.class);
-//            startActivity(i);
-//            //i.putExtra("user", u);
-//        }
-//        else {
-//            AlertDialog.Builder loginFail = new AlertDialog.Builder(this);
-//            loginFail.setMessage("Please fix errors and try again.");
-//            loginFail.setTitle("Error");
-//            loginFail.setPositiveButton("Close", null);
-//            loginFail.create().show();
-//        }
-//
-//        successSignup = true; // reset success flag
-//    }
+    public void onClickSignUp(View view) {
+        EditText usernameField = (EditText)findViewById(R.id.usernameSignupBox);
+        EditText passwordField = (EditText)findViewById(R.id.passwordSignupBox);
+        EditText nameField = (EditText)findViewById(R.id.nameSignupBox);
+        EditText emailField = (EditText)findViewById(R.id.emailSignupBox);
+        String username = usernameField.getText().toString();
+        String password = passwordField.getText().toString();
+        String name = nameField.getText().toString();
+        String email = emailField.getText().toString();
+        if (username.length() == 0) {
+            usernameField.setError("Please enter your username.");
+            successSignup = false;
+        }
+        // ELSE IF USERNAME EXISTS IN DB, ERROR
+
+        if (password.length() == 0) {
+            passwordField.setError("Please enter your password.");
+            successSignup = false;
+        }
+        else if (password.length() < 8) {
+            passwordField.setError("Password must be at least 8 characters.");
+            successSignup = false;
+        }
+        if (name.length() == 0) {
+            nameField.setError("Please enter your name.");
+            successSignup = false;
+        }
+        if(email.length() == 0) {
+            emailField.setError("Please enter your email.");
+            successSignup = false;
+        }
+        // ELSE IF EMAIL EXISTS IN DB, ERROR
+
+        if (successSignup) {
+            signupUser = new User(name, email, username, password);
+            // ADD USER TO DATABASE AND GET USER ID
+            // User u = GET USER FROM DB BY USER ID
+            Intent i = new Intent(LoginActivity.this, ProfileActivity.class);
+            startActivity(i);
+            //i.putExtra("user", u);
+        }
+        else {
+            AlertDialog.Builder loginFail = new AlertDialog.Builder(this);
+            loginFail.setMessage("Please fix errors and try again.");
+            loginFail.setTitle("Error");
+            loginFail.setPositiveButton("Close", null);
+            loginFail.create().show();
+        }
+
+        successSignup = true; // reset success flag
+    }
 
 }
